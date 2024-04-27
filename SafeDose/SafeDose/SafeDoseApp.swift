f56ab77ca1b80da6ae5b84a3ee28d8cea08da4c7
macintAsh1984 <ashh.valdez@gmail.com>
2024-04-27 16:01:20 -0700

    Uploaded Xcode project.

---
+++ SafeDose/SafeDose/SafeDoseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SafeDoseApp.swift
+//  SafeDose
+//
+//  Created by Ashley Valdez on 4/27/24.
+//
+
+import SwiftUI
+
+@main
+struct SafeDoseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
