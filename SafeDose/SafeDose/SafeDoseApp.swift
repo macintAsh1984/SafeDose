84cd9c8bfb4c088e1732cab7df0efc828b37f0e4
Shubhada Martha <103222835+smartha2003@users.noreply.github.com>
2024-04-27 19:02:59 -0700

    Info on active ingredients

--- SafeDose/SafeDose/SafeDoseApp.swift
+++ SafeDose/SafeDose/SafeDoseApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SafeDoseApp: App {
     var body: some Scene {
         WindowGroup {
-            EnterMedicationName()
+            ActiveIngredientsScanInfo()
         }
     }
 }
